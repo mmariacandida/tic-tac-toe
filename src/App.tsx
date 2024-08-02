d21dc4dc2272bd00b8756d787a859b24965ef32e
Milton Shyon de Lima Gurgel <milton.shyon@academico.ifrn.edu.br>
2024-08-02 16:03:13 -0300

    funcao onclick

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 
+import { useState } from 'react';
 import './App.css'
-const Square = ({value}) => {
-  const valor = value ?? "-";
-
-// function App() {
-//     return(
-//     <div>
-//       <h1>titulo</h1>
-//       <button className="square">X</button>
-//     </div>
-//     )
-//   }
-// let valor;
-// const Square = (props) => {
-//   //const valor = props.value ?? "-";
-//   if (props.value) {
-//     valor = props.value;
-//   } else {
-//     valor = "-";
-//   }
-//   return(<button className="square">{props.value}</button>);
-// }
+const Square = () => {
+  const [valor, setValue] = useState(null);
+  function handleClick() {
+    setValue('X');
+  }
+  // function App() {
+  //     return(
+  //     <div>
+  //       <h1>titulo</h1>
+  //       <button className="square">X</button>
+  //     </div>
+  //     )
+  //   }
+  // let valor;
+  // const Square = (props) => {
+  //   //const valor = props.value ?? "-";
+  //   if (props.value) {
+  //     valor = props.value;
+  //   } else {
+  //     valor = "-";
+  //   }
+  //   return(<button className="square">{props.value}</button>);
+  // }
   return (
-    <button className="square">
+    <button className="square" onClick={handleClick}>
       {valor}
     </button>
   );
@@ -30,21 +33,21 @@ const Square = ({value}) => {
 const App = () => {
   return (
     <>
-    <div className="board-row">
-      <Square value="X"/>
-      <Square value="6"/>
-      <Square />
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
       </div>
-    <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
-    </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   );
 }
